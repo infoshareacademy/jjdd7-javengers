5c516656357cdf8dfbc6f07b146996966381e75a
Andrzej Szypulski <szypul86@gmail.com>
2019-08-06 23:21:50 +0200

    test parser working as outer metod

--- src/main/java/com/infoshareacademy/Parser/Parser.java
+++ src/main/java/com/infoshareacademy/Parser/Parser.java
@@ -1,8 +1,8 @@
 package com.infoshareacademy.Parser;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.Date;
-
 
 import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.databind.JsonMappingException;
@@ -11,24 +11,26 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Parser {
 
-    public static void main(String[] args)
-    {
-        ParserTest drink = null;
+    public static void main(String[] args) {
+        ParserTest parserTest = null;
+        parseFile("/home/andrzej/javengers/jjdd7-javengers/src/main/resources/drinks.json", parserTest);
+
+    }
+    //z pliku ujebalem poczatkowa i koncowa klamre bo mi sie parser wykrzaczal - moze mozna to jakos obejsc??
+    //Czy ma zwracac voida czy jednak jakiegos returna??
+    public static void parseFile(String parsedFilePath, Object outputObject) {
+        outputObject = null;
         ObjectMapper mapper = new ObjectMapper();
-        try
-        {
-            drink =  mapper.readValue(new File("/home/andrzej/javengers/jjdd7-javengers/src/main/resources/drinks.json"), ParserTest.class);
-        } catch (JsonGenerationException e)
-        {
+        try {
+            outputObject = mapper.readValue(new File(parsedFilePath), ParserTest.class);
+        } catch (JsonGenerationException e) {
             e.printStackTrace();
-        } catch (JsonMappingException e)
-        {
+        } catch (JsonMappingException e) {
             e.printStackTrace();
-        } catch (IOException e)
-        {
+        } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println(drink);
+        System.out.println(outputObject);
     }
 
 
--- src/main/java/com/infoshareacademy/Parser/ParserTest.java
+++ src/main/java/com/infoshareacademy/Parser/ParserTest.java
@@ -1,12 +1,10 @@
 package com.infoshareacademy.Parser;
-import com.fasterxml.jackson.core.JsonGenerationException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-@JsonIgnoreProperties({"drinks","firstName", "strDrinkAlternate","strDrinkES","strDrinkDE","strDrinkFR","strDrinkZH-HANS","strDrinkZH-HANT","strTags","strVideo","strCategory","strIBA","strAlcoholic","strGlass","strInstructions","strInstructionsES","strInstructionsDE","strInstructionsFR","strInstructionsZH-HANS","strInstructionsZH-HANT","strDrinkThumb","strIngredient1","strIngredient2","strIngredient3","strIngredient4","strIngredient5","strIngredient6","strIngredient7","strIngredient8","strIngredient9","strIngredient10","strIngredient11","strIngredient12","strIngredient13","strIngredient14","strIngredient15","strMeasure1","strMeasure2","strMeasure3","strMeasure4","strMeasure5","strMeasure6","strMeasure7","strMeasure8","strMeasure9","strMeasure10","strMeasure11","strMeasure12","strMeasure13","strMeasure14","strMeasure15","strCreativeCommonsConfirmed","dateModified"})
 
+@JsonIgnoreProperties({"drinks", "firstName", "strDrinkAlternate", "strDrinkES", "strDrinkDE", "strDrinkFR", "strDrinkZH-HANS", "strDrinkZH-HANT", "strTags", "strVideo", "strCategory", "strIBA", "strAlcoholic", "strGlass", "strInstructions", "strInstructionsES", "strInstructionsDE", "strInstructionsFR", "strInstructionsZH-HANS", "strInstructionsZH-HANT", "strDrinkThumb", "strIngredient1", "strIngredient2", "strIngredient3", "strIngredient4", "strIngredient5", "strIngredient6", "strIngredient7", "strIngredient8", "strIngredient9", "strIngredient10", "strIngredient11", "strIngredient12", "strIngredient13", "strIngredient14", "strIngredient15", "strMeasure1", "strMeasure2", "strMeasure3", "strMeasure4", "strMeasure5", "strMeasure6", "strMeasure7", "strMeasure8", "strMeasure9", "strMeasure10", "strMeasure11", "strMeasure12", "strMeasure13", "strMeasure14", "strMeasure15", "strCreativeCommonsConfirmed", "dateModified"})
 
+// klasy do parsera musza spelniac zasady pojo!!
 public class ParserTest {
     private String idDrink;
     private String strDrink;
