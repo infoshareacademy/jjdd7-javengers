d5cfad1990379173bdc570886208b790455e5106
Rafał Misiak <rafalmisiak@gmail.com>
2019-08-06 09:34:22 +0200

    Update App.java

--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -1,6 +1,9 @@
 package com.infoshareacademy;
 
 public class App {
+    
+    private static final String LOREM_IPSUM = "lorem ipsum";
+    
     public static void main(String[] args) {
         System.out.println("Javengers! very long line very long line very long line very long line very long line very long line very long line very long line very long line very long line");
     }
