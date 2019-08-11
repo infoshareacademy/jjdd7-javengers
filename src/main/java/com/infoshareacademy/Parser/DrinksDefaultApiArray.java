804d8d9b04ee1fb61d77772023770059af531768
Andrzej Szypulski <48968331+szypul86@users.noreply.github.com>
2019-08-11 21:19:23 +0200

    Jjdd7 jv 6 (#12)
    * test parser working in main
    
    * test parser working as outer metod
    
    * parser test working for test class
    
    * Parser working for json arraylist
    
    * initial position for UserResponse implementation
    
    * coding coding testing testing
    
    * Parser ready for implementation
    
    * review remarks corrected
    
    * review remarks resolved
    
    * merged with parser
    
    *  review request resolved
    
    * hopefully last commit of task 6 in this sprint

---
+++ src/main/java/com/infoshareacademy/Parser/DrinksDefaultApiArray.java
@@ -0,0 +1,26 @@
+package com.infoshareacademy.Parser;
+
+import java.util.ArrayList;
+
+public class DrinksDefaultApiArray {
+
+    //class Field
+    private ArrayList<RecipeDTO> drinks;
+
+    //getters & setters
+    //if used for other text file need to change the line below!!
+    public ArrayList<RecipeDTO> getDrinks() {
+        return drinks;
+    }
+
+    public void setDrinks(ArrayList<RecipeDTO> drinks) {
+        this.drinks = drinks;
+    }
+
+    @Override
+    public String toString() {
+        return "ArrayParserTest{" +
+                "drinks='" + drinks + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/infoshareacademy/Parser/Parser.java
@@ -0,0 +1,40 @@
+package com.infoshareacademy.Parser;
+
+
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+
+
+public class Parser {
+
+    public static void main(String[] args) {
+        parseFile("drinks.json", DrinksDefaultApiArray.class);
+    }
+
+    public static <T>Object parseFile(String jsonFilePath, Class<T> classToCampare) {
+
+        T outputObject = null;
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+
+            outputObject = mapper.readValue(new File(jsonFilePath), classToCampare);
+        } catch (JsonGenerationException e) {
+            e.printStackTrace();
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    return outputObject;
+    }
+
+
+}
+
