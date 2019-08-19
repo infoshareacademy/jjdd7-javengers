e6c79a5523a0739d541ab7b1c278db074c879a85
Klaudia <klaudias.nowak@gmail.com>
2019-08-19 23:33:25 +0200

    App.java changed

--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -4,20 +4,20 @@ import com.infoshareacademy.menu.ChoiceReader;
 import com.infoshareacademy.menu.MenuManager;
 import com.infoshareacademy.menu.MenuPrinter;
 import com.infoshareacademy.properties.ConfigLoader;
-
 import java.io.IOException;
 
 public class App {
-    public static void main(String[] args) throws IOException {
-        System.out.println("\n\t\t\t\t\tWelcome to Drinkopedia!");
-        MenuManager menuManager = new MenuManager();
-        ChoiceReader choiceReader = new ChoiceReader();
-        MenuPrinter menuPrinter = new MenuPrinter();
-        menuPrinter.printEntryMenu();
-        int choice = choiceReader.makeMenuChoice();
-        menuManager.chooseMainMenuOption(choice);
 
-        ConfigLoader configLoader = new ConfigLoader();
-        configLoader.loadAppConfig();
-    }
+  public static void main(String[] args) throws IOException {
+    System.out.println("\n\t\t\t\t\tWelcome to Drinkopedia!");
+    MenuManager menuManager = new MenuManager();
+    ChoiceReader choiceReader = new ChoiceReader();
+    MenuPrinter menuPrinter = new MenuPrinter();
+    menuPrinter.printEntryMenu();
+    int choice = choiceReader.makeMenuChoice();
+    menuManager.chooseMainMenuOption(choice);
+
+    new ConfigLoader().loadAppConfig();
+
+  }
 }
