866d41d9e60f297208522fb4d70080095e54314a
Klaudia <klaudias.nowak@gmail.com>
2019-08-25 22:47:55 +0200

    new pom.xml and test file added

---
+++ src/test/java/com/infoshareacademy/AppConfigTest.java
@@ -0,0 +1,21 @@
+package com.infoshareacademy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIOException;
+
+import java.io.File;
+import org.junit.Test;
+
+public class AppConfigTest {
+  @Test
+  public void ShouldConfigFileExists() {
+    File file = new File("confg.properties");
+    if (!file.exists()) {
+      assertThatIOException();
+    } else {
+      assertThat(file.exists());
+    }
+
+  }
+
+}
