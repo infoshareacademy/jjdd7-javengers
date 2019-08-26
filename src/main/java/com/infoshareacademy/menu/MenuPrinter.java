a48e9e3170d096cafa9915b4b6a299936dc53522
Andrzej Szypulski <48968331+szypul86@users.noreply.github.com>
2019-08-26 12:44:44 +0200

    Jjdd7 jv 10 (#28)
    * listing by ingredients bug fixed
    
    * making printouts sexy
    
    * graphic changes
    
    * bug fixing
    
    * 18yrs bug fixed

--- src/main/java/com/infoshareacademy/menu/MenuPrinter.java
+++ src/main/java/com/infoshareacademy/menu/MenuPrinter.java
@@ -1,6 +1,7 @@
 package com.infoshareacademy.menu;
 
 import com.infoshareacademy.service.ClearScreenService;
+import org.apache.commons.text.WordUtils;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -11,7 +12,7 @@ public class MenuPrinter {
 
         System.out.println(GraphicContentFromFile.getGraphic("graphic.txt", StandardCharsets.US_ASCII));
         System.out.println
-                ("=================================== MENU ===================================" +
+                ("\n=================================== MENU ===================================" +
                         "\nEnter 1 to search drink by name" +
                         "\nEnter 2 to search drink by category" +
                         "\nEnter 3 to search drink by ingredient" +
@@ -24,10 +25,10 @@ public class MenuPrinter {
 
     }
 
-    void printMenuForDrinksByName(){
+    void printMenuForDrinksByName() {
 
         System.out.println
-                ("============================= DRINKS LIST MENU =============================" +
+                ("\n============================= DRINKS LIST MENU =============================" +
                         "\nEnter 1 to go back to main menu" +
                         "\nEnter 2 to exit" +
                         "\nOr enter a drink name (or at least 3 characters to find matching recipe):" +
@@ -35,21 +36,23 @@ public class MenuPrinter {
     }
 
 
-    void printMenuForDrinksByList(String listName){
+    void printMenuForDrinksByList(String listName) {
 
-        System.out.println
-                ("============================= DRINKS LIST MENU =============================" +
-                        "\nEnter 1 to back to main menu" +
-                        "\nEnter 2 to exit" +
-                        "\nOr enter List No or " + listName + " name(or more than one List No or " + listName +
-                        " divided by a colon ',') to find recipes: " +
-                        "\n============================================================================");
+        String instruction = "Or enter List No or " + listName +" name(or more than one List No or " + listName +" divi- ded by a colon ',') to find recipes:";
+        String wrappedInstruction = WordUtils.wrap(instruction, 77);
+
+
+        System.out.println("\n============================= DRINKS LIST MENU =============================" +
+                           "\nEnter 1 to back to main menu" +
+                           "\nEnter 2 to exit");
+        System.out.println(wrappedInstruction);
+        System.out.println("============================================================================");
     }
 
-    void printMenuForPickingARecipe(){
+    void printMenuForPickingARecipe() {
 
         System.out.println
-                ("============================= DRINKS LIST MENU =============================" +
+                ("\n============================= DRINKS LIST MENU =============================" +
                         "\nEnter 1 to back to main menu" +
                         "\nEnter 2 to exit" +
                         "\nEnter 3 to go upper in the menu" +
@@ -58,12 +61,10 @@ public class MenuPrinter {
     }
 
 
-
-
-    void printMenuForFavourites(){
+    void printMenuForFavourites() {
 
         System.out.println
-                ("============================= YOUR FAVOURITES ===============================" +
+                ("\n============================= YOUR FAVOURITES ===============================" +
                         "\nEnter 1 to back to main menu" +
                         "\nEnter 2 to exit" +
                         "\nEnter 3 [not implemented] toremove a drink from favourites list" +
@@ -71,17 +72,74 @@ public class MenuPrinter {
                         "\n============================================================================");
     }
 
-    void printMenuForRecipeView(String addOrRemove){
+    void printMenuForRecipeView(String addOrRemove) {
 
         System.out.println
-                ("============================== RECIPE MENU =================================" +
+                ("\n============================== RECIPE MENU =================================" +
                         "\nEnter 1 to back to main menu" +
                         "\nEnter 2 to exit" +
                         "\nEnter 3 to go upper in the menu" +
-                        "\nEnter 4 to "+ addOrRemove+" drink to favourites" +
+                        "\nEnter 4 to " + addOrRemove + " drink to favourites" +
                         "\nEnter 5 to delete a recipe from Drinkopedia" +
                         "\nEnter 6 to edit a recipe " +
                         "\n============================================================================");
+    }
+
+    void printBreadcrumb(String level1, String level2, String level3, String level4) {
+        System.out.println
+                (level1 + "\\" + level2 + "\\" + level3 + "\\" + level4 + "\\" +
+                        "\n============================================================================\n"
+                );
+    }
+
+    void printBreadcrumb(String level1, String level2, String level3) {
+        System.out.println
+                (level1 + "\\" + level2 + "\\" + level3 + "\\" +
+                        "\n============================================================================\n"
+                );
+    }
+
+    void printBreadcrumb(String level1, String level2) {
+        System.out.println
+                (level1 + "\\" + level2 + "\\" +
+                        "\n============================================================================\n"
+                );
+    }
+
+    void printBreadcrumb(String level1) {
+        System.out.println
+                (level1 + "\\" +
+                        "\n============================================================================\n"
+                );
+    }
+
+
+    void endview() {
+        System.out.println("\n============================================================================\n");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println("     A place ");
+        System.out.println("                        for ");
+        System.out.println("                                          your ");
+        System.out.println("                                                             advertisement ");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println("     A place ");
+        System.out.println("                        for ");
+        System.out.println("                                          your ");
+        System.out.println("                                                             advertisement ");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println("\n============================================================================\n");
+        System.out.println("                                                                by javengers ");
+
+
     }
 
 
