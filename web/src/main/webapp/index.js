b175c4a4815c469efc094a60f230b86eafaeccf2
Andrzej Szypulski <szypul86@gmail.com>
2019-09-05 00:44:25 +0200

    index.html WIP

--- web/src/main/webapp/index.js
+++ web/src/main/webapp/index.js
@@ -22,6 +22,32 @@ $formName.on('submit', () => {
     return false;
 });
 
+//filter names
+/*const searchBar = document.forms['form-name'].querySelector('input');
+searchBar.addEventListener('keyup',function(e)
+{
+
+    const term = e.target.value.toLowerCase();
+    const drinks = list.getElementsByTagName("li");
+    Array.from(drinks).forEach(function (drink) {
+        const name = drink.firstElementChild.textContent;
+        if (name.toLowerCase().indexOf(term) != -1) {
+            drink.style.display = 'block';
+        }
+    })
+else
+    {
+        drink.style.display = 'none';
+    }
+}
+);*/
+
+
+/**
+ *  INGRIDIENTS FORM AND MANAGING INGREDIENT BUTTONS
+ *  ____________________________________________
+ */
+
 $formIngredient.on('submit', () => {
     const $input = $("#input-ingredient");
     message = $input.val();
@@ -31,29 +57,25 @@ $formIngredient.on('submit', () => {
     const newTodo = makeTodoHtml(message);
     $todoList.prepend(newTodo);
 
+    radiobtn = document.getElementById(message);
+    radiobtn.checked = true;
+    selectIngredient()
+
     $input.val('');
     return false;
 });
 
-/**
- *  CHECKBOX HANDLING
- *  -------------------------------------------
- */
 
 $(document).on('click', '#list-todo label', function (event) {
     const element = $(event.target).parents('.form-group');
     element.remove();
 });
 
-/**
- *  UTILITY FUNCTIONS
- *  ____________________________________________
- */
 
 function makeTodoHtml(message) {
     return `
     <label class="btn btn-primary form-group">
-     <input class="" type="checkbox" name="myradio" value="">
+     <input class="x-ingredient" id="${message}" type="checkbox" name="myradio" value="${message}" onclick="selectIngredient()">
      <span class="form-check-label">${message}</span>
     </label>   
 `
@@ -73,8 +95,15 @@ $(document).on('click', '#navTab a', function () {
 });
 </script>*/
 
+
+/**
+ *  MANAGING SENDING DATA TO SERVLET
+ *  ____________________________________________
+ */
+
 let selectedCategories = []
-let asdasd= []
+let selectedListOptions = []
+let selectedIngredients = []
 
 function selectCategory () {
     const categories = $('.x-category')
@@ -87,13 +116,64 @@ function selectCategory () {
             }
         })
     }
-
     const queryParams = $.param({
         categories: selectedCategories,
-        drugiParam: ['s', 'sss']
+        listOptions: selectedListOptions,
+        ingredients: selectedIngredients
+
     })
 
-    fetch('http://localhost:63342/drinks?' + queryParams, {
+    fetch('http://localhost:8080/drinks?' + queryParams, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+}
+
+
+function selectListOption () {
+    const listOptions = $('.x-list-options')
+    if (listOptions && listOptions.length) {
+        selectedListOptions = []
+        listOptions.each(function(i) {
+            const input = this
+            if (input.checked) {
+                selectedListOptions.push(input.value)
+            }
+        })
+    }
+    const queryParams = $.param({
+        categories: selectedCategories,
+        listOptions: selectedListOptions,
+        ingredients: selectedIngredients
+    })
+
+    fetch('http://localhost:8080/drinks?' + queryParams, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+}
+
+function selectIngredient () {
+    const ingredients = $('.x-ingredient')
+    if (ingredients && ingredients.length) {
+        selectedIngredients = []
+        ingredients.each(function(i) {
+            const input = this
+            if (input.checked) {
+                selectedIngredients.push(input.value)
+            }
+        })
+    }
+    const queryParams = $.param({
+        categories: selectedCategories,
+        listOptions: selectedListOptions,
+        ingredients: selectedIngredients
+
+    })
+
+    fetch('http://localhost:8080/drinks?' + queryParams, {
         headers: {
             'Content-Type': 'application/json'
         }
